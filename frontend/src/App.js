b848b0bf7018703a2d54df838afb8f90ee2b3b8c
hung.nd <57054734+nguyen-dang-hung-20183547@users.noreply.github.com>
2021-04-26 23:49:26 +0700

    danghung frontend push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import FashionNews from "./components/Home/FashionNews/FashionNews";
+import Footer from "./components/Home/Footer/Footer";
+import Newsletter from "./components/Home/NewsLetter/NewsLetter";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <header className="App-header">
         <p>
           <FashionNews/>
+            <Newsletter/>
+            <Footer/>
         </p>
       </header>
     </div>
--- frontend/src/components/Home/FashionNews/FashionNews.js
+++ frontend/src/components/Home/FashionNews/FashionNews.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import '../../../App.css';
 import News from '../../News/News'
 import classNames from 'classnames'
 import axios from 'axios'
