b85139f6adb13a209b314e7f1b41786dbf4bd6aa
dai02012000 <ledai02012000@gmail.com>
2021-05-06 22:42:03 +0700

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ export default function Home() {
     
     return (
         <div className="Home">
-            <Banner/>
+            <Banner collection={collection}/>
             <RecommendBanner/>
             <HomeTab/>
             <Collection
