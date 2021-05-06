b74afae856f7bc79b98eb901419400b4ded4ce85
hung.nd <57054734+nguyen-dang-hung-20183547@users.noreply.github.com>
2021-05-06 22:07:46 +0700

    update css and home.js

--- frontend/src/components/Home/Footer/Footer.js
+++ frontend/src/components/Home/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebookF, faTwitter, faInstagram, faPinterest, faGoogle } from '@fortawesome/free-brands-svg-icons';
 // import { Link } from 'react-router-dom'
+import './Footer.css'
 
 
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,11 @@ import "../App.css"
 import axios from 'axios';
 import Collection from "../components/Home/Collection/Collection";
 import HomeTab from "../components/Home/HomeTab/HomeTab"
+import Banner from "../components/Home/Banner/Banner";
+import RecommendBanner from "../components/Home/RecommendBanner/RecommendBanner";
+import FashionNews from "../components/Home/FashionNews/FashionNews";
+import Newsletter from "../components/Home/NewsLetter/NewsLetter";
+import Footer from "../components/Home/Footer/Footer";
 
 export default function Home() {
     const [collection, setCollection] = useState([]);
@@ -17,10 +22,15 @@ export default function Home() {
     
     return (
         <div className="Home">
+            <Banner/>
+            <RecommendBanner/>
             <HomeTab/>
             <Collection
                 collection={collection}
             />
+            <FashionNews/>
+            <Newsletter/>
+            <Footer/>
         </div>
     )
 }
