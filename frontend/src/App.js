174324c84d4be9c21496fd7fe06e97853ffc1cf1
hung.nd <57054734+nguyen-dang-hung-20183547@users.noreply.github.com>
2021-05-06 17:08:06 +0700

    delete context

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
-// import Header from './components/Header/Header';
-// import FashionNews from "./components/Home/FashionNews/FashionNews";
-// import Footer from "./components/Home/Footer/Footer";
-import Newsletter from "./components/Home/NewsLetter/NewsLetter";
-import Banner from './components/Home/Banner/Banner';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import RecommendBanner from './components/Home/RecommendBanner/RecommendBanner';
+import Home from "./components/Home/Home";
 
 function App() {
-  const [collection, setCollection] = useState([]);
-  useEffect(() => {
-    axios.get(`http://localhost:4000/collection`)
-         .then(res => {
-           setCollection(res.data)
-         })
-    window.scrollTo(0, 0);
-    document.body.style.overflow = 'unset';
-    
-  }, []);
-
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <p> */}
-          
-          {/* <FashionNews/> */}
-            {/* <Newsletter/> */}
-            {/* <Footer/> */}
-          <Banner collection={collection}/>
-          <RecommendBanner/>
-          {/* <Recom */}
-        {/* </p> */}
-      {/* </header> */}
+      <header className="App-header">
+        <p>
+            <Home/>
+        </p>
+      </header>
     </div>
   );
 }
