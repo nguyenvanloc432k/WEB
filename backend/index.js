c3e6a52b96175f417d0a220f3caaa3a89a853a1b
boyonepiece <stah432987@gmail.com>
2021-05-12 23:40:05 +0700

    add cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 
-app.use(cors);
+app.use(cors());
 
 // router
 app.use('/products',Product);
