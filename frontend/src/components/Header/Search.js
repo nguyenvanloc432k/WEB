c246785d9f66b515e9e43e106b068b2b0654e435
Tuan Hien <986753421hn@gmail.com>
2021-05-15 11:40:50 +0700

    done Search

--- frontend/src/components/Header/Search.js
+++ frontend/src/components/Header/Search.js
@@ -4,29 +4,34 @@ import axios from 'axios'
 import { faCartPlus, faCheckCircle, faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 import './Search.css'
 import '../../App.css'
+import {withRouter} from 'react-router-dom'
 
-
-export default function Search(props) {
+function Search(props) {
     const [products, setProducts] = useState([])
     const [searchInput, setSearchInput] = useState("")
-
+    const [constProducts, setConstProducts] = useState([])
     useEffect(() => {
         axios.get(`http://localhost:4000/products`)
             .then(res => {
+                setConstProducts(res.data)
                 setProducts(res.data)
             })
     }, [])
 
     const search = (event) => {
         const value = event.target.value
-        setSearchInput(value)
         const search = []
-        for (let i in products) {
-            if ((products[i].productName).toLowerCase().includes(searchInput)) {
-                search.push(products[i])
+        for (let i in constProducts) {
+            if ((constProducts[i].productName).toLowerCase().includes(value.toLowerCase())) {
+                search.push(constProducts[i])
             }
         }
+        setSearchInput(value)
         setProducts(search)
+    }
+    const handleClick = (e) => {
+        const id = e.target.id
+        props.history.push(`products/${id}`)
     }
 
     return (
@@ -59,7 +64,24 @@ export default function Search(props) {
                         (products.length > 0 && searchInput !== "") &&
                         products.map((item, index) => {
                             return (
-                                                        
+                                <div className="search-item" key={index}>
+                                    <div className="search-item-img">
+                                        <img src={item.productImg} height="100%" alt=""/>
+                                    </div>
+                                    <div className="search-item-body">
+                                        <div className="search-item-name">
+                                            <p>{item.productName}</p>
+                                        </div>
+                                        <div className="item-view">
+                                            <div className="button"
+                                                id={item.productID}
+                                                onClick={handleClick}
+                                            >
+                                                VIEW
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             )
                         })
                     }
@@ -68,3 +90,5 @@ export default function Search(props) {
         </div>
     )
 }
+
+export default withRouter(Search)
